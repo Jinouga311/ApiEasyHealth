e19a77b0ce1f9754ba13ba2d1175c4cb7bc1002b
Jinouga311 <azzeddine.lahmar@zohomail.eu>
2023-04-13 01:33:54 +0200

    Trouve un médecin pour la requête qui affiche les infos d'un médecin, même quand ce dernier n'a aucune consultation qui lui est associé.

--- src/main/java/com/example/apiprojeteasyhealth/repository/MedecinRepository.java
+++ src/main/java/com/example/apiprojeteasyhealth/repository/MedecinRepository.java
@@ -84,61 +84,53 @@ public interface MedecinRepository extends JpaRepository<Medecin, Long> {
 
 
     //Afficher les détails d'un patient :
-    @Query("SELECT m.id as idMedecin, m.nom as medecinNom, m.adresseMail as adresseMail, m.numeroTelephone as numeroTelephone, m.specialite as specialite, m.cheminFichier as photoProfil, "
-            + "c.id as idConsultation, c.date as date, c.prix as prix, pa.libelle as pathologieLibelle, p.nom as patientNom, p.prenom as patientPrenom, p.adresseMail as patientMail, "
-            + " p.numeroTelephone as patientTel, s.description as suiviDescription, s.etat as suiviEtat, o.dateOrdo as ordonnanceDate, o.contenu as ordonnanceContenu, "
-            + "medicament.nom as medicamentNom, medicament.description as medicamentDescription, pr.quantite as prescriptionQuantite "
-            + "FROM Patient p "
-            + "LEFT JOIN p.consultations c "
-            + "LEFT JOIN c.pathologie pa "
-            + "LEFT JOIN c.medecin m "
-            + "LEFT JOIN c.suivis s "
-            + "LEFT JOIN c.ordonnances o "
-            + "LEFT JOIN o.prescriptions pr "
-            + "LEFT JOIN pr.medicament medicament "
+    @Query("SELECT m.id as idMedecin, m.nom as medecinNom, m.adresseMail as adresseMail, m.numeroTelephone as numeroTelephone, m.specialite as specialite, m.cheminFichier as photoProfil "
+            + "FROM Medecin m "
             + "WHERE m.adresseMail = :mail")
-    List<Tuple> findMedecinInfoByEmail(@Param("mail") String adresseMail);
+    Tuple findMedecinInfoByEmail(@Param("mail") String adresseMail);
+
+    @Query("SELECT c.id as idConsultation, c.date as date, c.prix as prix, pa.libelle as pathologieLibelle, p.nom as patientNom, p.prenom as patientPrenom, p.adresseMail as patientMail, "
+            + " p.numeroTelephone as patientTel "
+            + "FROM Consultation c "
+            + "LEFT JOIN c.pathologie pa "
+            + "LEFT JOIN c.patient p "
+            + "LEFT JOIN c.medecin m "
+            + "WHERE m.adresseMail = :mail")
+    List<Tuple> findConsultationsForMedecin(@Param("mail") String adresseMail);
+
 
     public default List<AllAboutMedecin> getMedecinInfoByEmail(String adresseMail) {
-        List<Tuple> rawData = findMedecinInfoByEmail(adresseMail);
-        Map<Long, AllAboutMedecin> resultMap = new LinkedHashMap<>();
-        Map<Long, Long> consultationIdMap = new HashMap<>();
+        Tuple medecinTuple = findMedecinInfoByEmail(adresseMail);
+        if (medecinTuple == null) {
+            return Collections.emptyList();
+        }
+        List<Tuple> consultationTuples = findConsultationsForMedecin(adresseMail);
+        AllAboutMedecin allAboutMedecin = new AllAboutMedecin(
+                medecinTuple.get("idMedecin", Long.class),
+                medecinTuple.get("medecinNom", String.class),
+                medecinTuple.get("adresseMail", String.class),
+                medecinTuple.get("numeroTelephone", String.class),
+                medecinTuple.get("specialite", String.class),
+                medecinTuple.get("photoProfil", String.class),
+                new ArrayList<>()
+        );
+
         long genericConsultationId = 1;
-
-        for (Tuple row : rawData) {
-            Long idMedecin = row.get("idMedecin", Long.class);
-            AllAboutMedecin allAboutMedecin = resultMap.computeIfAbsent(idMedecin, k -> {
-                long idMedecinInfo = resultMap.size() + 1;
-                return new AllAboutMedecin(
-                        idMedecinInfo,
-                        row.get("medecinNom", String.class),
-                        row.get("adresseMail", String.class),
-                        row.get("numeroTelephone", String.class),
-                        row.get("specialite", String.class),
-                        row.get("photoProfil", String.class),
-                        new ArrayList<>()
-                );
-            });
-
-            Long idConsultation = row.get("idConsultation", Long.class);
-            if(idConsultation != null && !consultationIdMap.containsKey(idConsultation)){
-                consultationIdMap.put(idConsultation, genericConsultationId);
-                ConsultationInformationsForMedecin consuultation = new ConsultationInformationsForMedecin(
-                        genericConsultationId++,
-                        row.get("patientNom", String.class),
-                        row.get("patientPrenom", String.class),
-                        row.get("patientMail", String.class),
-                        row.get("patientTel", String.class),
-                        row.get("date", LocalDate.class),
-                        row.get("prix", Float.class),
-                        row.get("pathologieLibelle", String.class)
-
-                );
-                allAboutMedecin.getConsultations().add(consuultation);
-            }
-
+        for (Tuple row : consultationTuples) {
+            ConsultationInformationsForMedecin consuultation = new ConsultationInformationsForMedecin(
+                    genericConsultationId++,
+                    row.get("patientNom", String.class),
+                    row.get("patientPrenom", String.class),
+                    row.get("patientMail", String.class),
+                    row.get("patientTel", String.class),
+                    row.get("date", LocalDate.class),
+                    row.get("prix", Float.class),
+                    row.get("pathologieLibelle", String.class)
+            );
+            allAboutMedecin.getConsultations().add(consuultation);
         }
 
-        return new ArrayList<>(resultMap.values());
+        return Collections.singletonList(allAboutMedecin);
     }
+
 }
